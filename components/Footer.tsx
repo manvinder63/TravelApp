7c920ff93bb43ca0b56ccefc312a11e9cf4074d1
manvinder63 <manvinder1963@outlook.com>
2024-04-29 23:51:21 +1000

    Implemented Get app component and started Footer Section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container flex w-full
+      flex-col gap-14'>
+        <div className='flex flex-col items-start justify-center gap[10%] md:flex-row'>
+            <Link href="/" className="mb-10">
+            </Link>
+        </div>
+      </div>
+    </footer>
   )
 }
 
